3e263bf058f4cf6f0450ba6b23e93e782547822f
James Pickles <picklesj85@gmail.com>
2016-02-08 19:12:58 +0000

    made file name final

--- CurrentDirectory.java
+++ CurrentDirectory.java
@@ -1,11 +1,14 @@
 import  java.io.File;
 
 public class CurrentDirectory {
+	private static final String AbsFileName = "\\My Documents\\PiJ Work\\Week-15-Exercises";
+	
 	public static void main(String[] args) {
-		File file = new File("\\My Documents\\PiJ Work\\Week-15-Exercises");
+		File file = new File(AbsFileName);
 		String[] list = file.list();
 		for (int i = 0; i < list.length; i++) {
 			System.out.println(list[i]);
 		}
+		
 	}
 }
